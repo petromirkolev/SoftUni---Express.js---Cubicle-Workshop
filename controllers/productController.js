b9cad015e2626c63f729b3f21cb3dfbf375087ad
petromirkolev <petromirkolev@gmail.com>
2021-01-30 13:33:41 +0200

    Add page titles

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,15 +2,11 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {title: 'Home'});
 });
 
 router.get('/create', (req, res) => {
-    res.render('create');
-}); 
-
-router.get('/about', (req, res) => {
-    res.render('about');
+    res.render('create', { title: 'Create'});
 }); 
 
 module.exports = router ; 
