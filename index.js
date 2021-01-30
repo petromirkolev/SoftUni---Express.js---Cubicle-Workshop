1099fff670ef744836d0070ae16713b5102e1c1b
petromirkolev <petromirkolev@gmail.com>
2021-01-30 12:40:53 +0200

    Add router

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const config = require('./config/config.js'); 
 const app = express();
+const routes = require('./routes'); 
 require('./config/express')(app);
- 
-app.get('/', (req, res) => {
-    res.render('home', {layout: false});
-})
+
+app.use(routes);
 
 app.listen(config.PORT, () => {
     console.log(`Server is running on port ${config.PORT}...`);
