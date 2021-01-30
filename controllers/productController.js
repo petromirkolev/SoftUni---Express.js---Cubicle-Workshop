cfafba8905065c3ca1596c9af77354ad9d3016c4
petromirkolev <petromirkolev@gmail.com>
2021-01-30 13:50:36 +0200

    Change About to Home controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,11 +2,11 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('home', {title: 'Home'});
-});
+    res.render('home', { title: 'Home' });
+}); 
 
 router.get('/create', (req, res) => {
     res.render('create', { title: 'Create'});
 }); 
 
-module.exports = router ; 
+module.exports = router; 
--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
 const productController = require('./controllers/productController');
-const aboutController = require('./controllers/aboutController');
+const homeController = require('./controllers/homeController');
 const router = Router();
 
-router.use('/', productController);
-router.use('/about', aboutController);
+router.use('/', homeController);
+router.use('/products', productController);
 router.get('*', (req, res) => {
     res.render('404');
 })
