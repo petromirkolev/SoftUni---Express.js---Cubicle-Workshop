492ab2d1966e555f4471a5449974b854c2d68a34
petromirkolev <petromirkolev@gmail.com>
2021-01-30 13:17:50 +0200

    Add about controller

---
+++ controllers/productController.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.render('home', { layout: false });
+});
+
+router.get('/create', (req, res) => {
+    res.render('create', { layout: false });
+}); 
+
+router.get('/about', (req, res) => {
+    res.render('about', { layout: false });
+}); 
+
+module.exports = router ; 
