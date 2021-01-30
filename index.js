aad48e31eec512315dcf1ec138e1c3a73582cfcc
petromirkolev <petromirkolev@gmail.com>
2021-01-30 12:30:00 +0200

    Create HBS config file

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const config = require('./config/config.js'); 
 const app = express();
-const handlebars = require('express-handlebars');
-
-app.engine('hbs', handlebars({
-    extname: 'hbs', 
-}));
-app.set('view engine', 'hbs');
-
+require('./config/express')(app);
+ 
 app.get('/', (req, res) => {
     res.render('home', {layout: false});
 })
